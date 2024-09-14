468a9a9e80a84e8cf4105486fb3c5414f3c83843
Burnt Chrome <BurntChromium@outlook.com>
2024-09-14 01:11:39 -0400

    insertion first draft done

--- src/filter.rs
+++ src/filter.rs
@@ -3,56 +3,215 @@
 //! This implementation is based on the paper _Cuckoo Filter: Practically Better Than Bloom_, by Fan et. al.
 //!
 //! The paper recommends a (2, 4) CF (2 possible buckets for each item, and 4 fingerprints in each bucket) because it's space optimal for practical false positive rates. Assuming our CF will hold up to a few billion items, 6 bits per fingerprint is sufficient (24 bits per bucket), but we round up to one byte per fingerprint for the sake of practicality.
+//!
+//! The paper's authors have provided a reference C++ implementation in this repository: https://github.com/efficient/cuckoofilter
 
-/// Each fingerprint is 1 byte
-const BITS_PER_ITEM: usize = 8;
-// Each bucket holds 4 fingerprints
+use crate::hash;
+
+type Input = [u8];
+type BucketIndex = u32;
+type Fingerprint = u8;
+
+const MAX_EVICTIONS: u16 = 500;
+/// Each bucket holds 4 fingerprints
 const BUCKET_SIZE: usize = 4;
-// We support up to u32 buckets, which means we can hold `u32::MAX * 4` items
+/// We support up to u32 buckets, which means we can hold `u32::MAX * 4` items
 const ITEM_LIMIT: usize = u32::MAX as usize * BUCKET_SIZE as usize;
+/// Easily swap hash functions during development, TODO: pick one
+const HASH_FN: fn(&[u8]) -> u32 = hash::hash_djb2;
 
 /// We need to cache items that have been evicted so we can relocate them, this is the shape of that cache
 struct EvictionVictim {
     index: u32,
-    tag: u8,
+    fingerprint: Fingerprint,
     used: bool,
 }
 
 impl EvictionVictim {
-    pub fn new() -> EvictionVictim {
+    fn new() -> EvictionVictim {
         EvictionVictim {
             index: 0,
-            tag: 0,
+            fingerprint: 0,
             used: false,
         }
     }
+
+    fn reset(&mut self) {
+        self.index = 0;
+        self.fingerprint = 0;
+        self.used = false;
+    }
+}
+
+/// An error that can result at "runtime" when performing insert/delete operations
+pub enum CuckooFilterOpError {
+    OutOfSpace,
+    ItemAlreadyExists,
+    ItemDoesNotExist,
 }
 
 /// A Cuckoo Filter that holds up to 17 billion items
 pub struct CuckooFilter {
     eviction_cache: EvictionVictim,
-    data: Vec<[u8; BUCKET_SIZE]>,
+    data: Vec<[Fingerprint; BUCKET_SIZE]>,
+    length_u32: u32,
 }
 
 impl CuckooFilter {
+    /// Try to create a new Cuckoo Filter
+    ///
+    /// This can fail if the desired filter would be too large.
+    ///
+    /// ### Caveats
+    ///
+    /// - We must round the size of our backing vector of data to a power of two. This is because we will modulo the index when our hash function creates a bucket index bigger than the backing vector. If the data was *not* a power of 2, our indices would be subject to "Modulo bias" and cause more hash collisions.
     pub fn new(max_items: usize) -> Result<CuckooFilter, String> {
+        // Check item limit
         if max_items > ITEM_LIMIT {
             return Err(format!("requested cuckoo filter exceeds practical constraints, more than {ITEM_LIMIT} items is not supported"));
         }
-        let number_of_buckets: usize = max_items / BUCKET_SIZE;
+        // If we didn't care about modulo bias, we could use this many buckets
+        let number_of_buckets_exact: usize = max_items / BUCKET_SIZE;
+        // But to avoid hash collisions, we round up
+        let number_of_buckets_actual: usize = number_of_buckets_exact.next_power_of_two();
+        // Check for overflow after rounding to next power of two
+        if number_of_buckets_actual > u32::MAX as usize {
+            return Err(format!("requested cuckoo filter would exceed size constraints due to power of two rounding: requested items requires {number_of_buckets_actual} buckets, which is the next highest power of 2."));
+        }
         Ok(CuckooFilter {
             eviction_cache: EvictionVictim::new(),
-            data: Vec::with_capacity(number_of_buckets),
+            data: Vec::with_capacity(number_of_buckets_actual),
+            length_u32: number_of_buckets_actual as u32,
         })
     }
 
-    /// Compute a 1 byte fingerprint from the hash value
-    pub fn fingerprint(hash_value: u32) -> u8 {
-        (hash_value % (1 << BITS_PER_ITEM - 1) + 1) as u8
+    /// Approximately how many bytes is this CF using?
+    pub fn estimate_size(&self) -> usize {
+        self.data.len() * BUCKET_SIZE
     }
 
-    /// Approximately how many bytes is this CF using?
-    pub fn size_estimate(&self) -> usize {
-        self.data.len() * BUCKET_SIZE
+    /// Is the Cuckoo Filter full of items?
+    ///
+    /// Criteria is that we have something left over in the Eviction cache after trying to move it for the max number of kicks
+    pub fn is_full(&self) -> bool {
+        self.eviction_cache.used
+    }
+
+    /// Calculate the buckets given an actual input item
+    ///
+    /// We modulo the bucket indices because the hash may output a value larger than the true length of the backing data array. However, because our length is a power of 2, we can use bitwise AND.
+    ///
+    /// This is (mostly) Equation 1 in section 3.1 of the paper
+    ///
+    /// However, unlike Equation 1, we follow the reference implementation from the authors and instead compute bucket 2 by XORing with a magic constant
+    fn buckets_from_item(&self, item: &Input) -> (BucketIndex, BucketIndex, Fingerprint) {
+        let bucket_1 = HASH_FN(item) & (self.length_u32 - 1);
+        // The magic constant is from MurmurHash2 (as in the reference impl)
+        let bucket_2 = bucket_1 ^ (hash::byte_fingerprint_long(bucket_1) * 0x5bd1e995);
+        (
+            bucket_1,
+            bucket_2 & (self.length_u32 - 1),
+            hash::byte_fingerprint_short(bucket_1),
+        )
+    }
+
+    /// We can calculate a new bucket for an evicted item despite only having that item's fingerprint
+    ///
+    /// This is Equation 2 in Section 3.1 of the paper
+    fn bucket_from_evicted(
+        &self,
+        old_bucket: BucketIndex,
+        fingerprint: Fingerprint,
+    ) -> BucketIndex {
+        (old_bucket ^ (fingerprint as u32)) & (self.length_u32 - 1)
+    }
+
+    /// Internal method to try inserting a fingerprint into a bucket.
+    ///
+    /// True means success, false means the bucket was full
+    fn try_insert_at_bucket(
+        &mut self,
+        bucket_index: BucketIndex,
+        fingerprint: Fingerprint,
+    ) -> bool {
+        let mut bucket = self.data[bucket_index as usize];
+        for f_print in bucket.iter_mut() {
+            if *f_print == 0 {
+                *f_print = fingerprint;
+                return true;
+            }
+        }
+        false
+    }
+
+    /// Internal method to swap an existing fingerprint for a new one (the Cuckoo mechanism)
+    fn swap_at_bucket(
+        &mut self,
+        bucket_index: BucketIndex,
+        fingerprint: Fingerprint,
+        slot: usize,
+    ) -> Fingerprint {
+        let mut bucket = self.data[bucket_index as usize];
+        let evicted_fingerprint = bucket[slot];
+        bucket[slot] = fingerprint;
+        evicted_fingerprint
+    }
+
+    /// Add item to filter. Returns Err if filter is full
+    pub fn insert(&mut self, item: &Input) -> Result<(), CuckooFilterOpError> {
+        if self.eviction_cache.used {
+            return Err(CuckooFilterOpError::OutOfSpace);
+        }
+
+        let (candidate_1, candidate_2, fingerprint) = self.buckets_from_item(item);
+
+        // Try inserting into either bucket
+        for &bucket_index in &[candidate_1, candidate_2] {
+            if self.try_insert_at_bucket(bucket_index, fingerprint) {
+                return Ok(());
+            }
+        }
+
+        // If both buckets are full, begin eviction process
+        let mut target_bucket_index = if fingerprint % 2 == 0 {
+            candidate_1
+        } else {
+            candidate_2
+        };
+        let mut evicted_fingerprint: u8 = 0;
+
+        for kick in 0..MAX_EVICTIONS {
+            if kick > 0 && self.try_insert_at_bucket(target_bucket_index, fingerprint) {
+                return Ok(());
+            }
+
+            // Randomly choose a slot to evict from and swap
+            let slot = (target_bucket_index % BUCKET_SIZE as u32) as usize;
+            evicted_fingerprint = self.swap_at_bucket(target_bucket_index, fingerprint, slot);
+
+            // Recalculate the next target bucket based on the evicted fingerprint
+            target_bucket_index =
+                self.bucket_from_evicted(target_bucket_index, evicted_fingerprint);
+        }
+        // If MAX_EVICTIONS is reached, store the fingerprint in the eviction cache
+        self.eviction_cache.index = target_bucket_index;
+        self.eviction_cache.fingerprint = evicted_fingerprint;
+        self.eviction_cache.used = true;
+        Err(CuckooFilterOpError::OutOfSpace)
+    }
+
+    /// Add item to filter. Returns Err if filter is full, or if item already exists.
+    pub fn insert_unique(item: &Input) -> Result<(), CuckooFilterOpError> {
+        Ok(())
+    }
+
+    /// Check if item is in filter
+    pub fn lookup(item: &Input) -> bool {
+        false
+    }
+
+    /// Delete an item from the filter
+    pub fn delete(item: &Input) -> Result<(), CuckooFilterOpError> {
+        Ok(())
     }
 }
--- src/hash.rs
+++ src/hash.rs
@@ -21,3 +21,38 @@ pub fn hash_sbdm(input: &[u8]) -> u32 {
     }
     hash
 }
+
+/// Compute a 1 byte fingerprint from a hash digest but emit as 32 bits for XORing
+///
+/// As in the C++ reference implementation, the fingerprint cannot be zero
+pub fn byte_fingerprint_long(hash_value: u32) -> u32 {
+    let fingerprint = hash_value % (1 << 8 - 1);
+    // Prevent a fingerprint of 0 (because 0 implies empty bucket)
+    fingerprint + (fingerprint == 0) as u32
+}
+
+/// Compute a 1 byte fingerprint and truncate the empty bits
+pub fn byte_fingerprint_short(hash_value: u32) -> u8 {
+    byte_fingerprint_long(hash_value) as u8
+}
+
+/* -------------------- Unit Tests -------------------- */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_hash_test_djb2() {
+        let a = hash_djb2("cat".as_bytes());
+        let b = hash_djb2("dog".as_bytes());
+        assert_ne!(a, b);
+    }
+
+    #[test]
+    fn basic_hash_test_sbdm() {
+        let a = hash_sbdm("cat".as_bytes());
+        let b = hash_sbdm("dog".as_bytes());
+        assert_ne!(a, b);
+    }
+}
